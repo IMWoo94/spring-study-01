f287d7fcf166b05eed3e612862229579fc13cffd
IM_Woo <leemeawoo94@gamil.com>
2023-08-07 19:41:40 +0900

    2023.08.07 study spring DI

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -1,7 +1,15 @@
 package hello.hellospring;
 
+import hello.hellospring.controller.MemberController;
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class HelloSpringApplication {
