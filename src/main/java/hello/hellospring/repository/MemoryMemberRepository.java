a0dfae225e8eac6f53f1880d6bfcff7c14fa3bb8
IMWoo94 <leemeawoo94@gmail.com>
2023-08-12 20:07:38 +0900

    2023.08.12 spring db connect study

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -2,6 +2,7 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 import org.junit.jupiter.api.AfterEach;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,11 +5,13 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 //@Service
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -22,6 +24,7 @@ public class MemberService {
     /**
      * 회원가입
      */
+//    @Transactional
     public Long join(Member member){
         // 중복회원 검색
         validateDuplicateMember(member);
